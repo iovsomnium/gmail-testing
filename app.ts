cc850522fc7041ebd15efb38e16b581ae18dcfcd
iovsomnium <ksw09157.5212@gmail.com>
2024-04-23 17:10:51 +0900

    fix: edit wrong config and add comments

--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
 import { createTransport } from 'nodemailer';
 import config from './config';
 
+// 수신자 이메일 주소 설정
 const receiver: string = 'ksw09157@naver.com';
 
 interface RegisterUserDTO {
   email: string;
 }
 
+// 사용자 서비스 클래스 정의
 class UserService {
-  static register = async (userDTO: RegisterUserDTO) => {
+  // OAuth를 사용하여 메일을 보내는 static 메소드
+  static sendMailWithOAuth = async (userDTO: RegisterUserDTO) => {
     try {
+      // nodemailer의 createTransport를 사용하여 메일 전송자 설정
       const transporter = createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
@@ -24,19 +28,23 @@ class UserService {
         },
       });
 
+      // 메일 옵션 설정
       const mailOptions = {
         to: userDTO.email,
         subject: 'testing',
         html: 'for testing',
       };
 
+      // 메일 전송
       await transporter.sendMail(mailOptions);
     } catch (err) {
-      console.error(err);
+      console.error(err); // 에러 발생 시 콘솔에 출력
     }
   };
 
-  static sendMail = async () => {
+  // 비밀번호를 사용하여 메일을 보내는 static 메소드
+  static sendMailWithPassword = async () => {
+    // nodemailer의 createTransport를 사용하여 메일 전송자 설정(기본 인증 사용)
     const transporter = createTransport({
       service: 'gmail',
       auth: {
@@ -45,20 +53,21 @@ class UserService {
       },
     });
 
+    // 메일 옵션 설정
     const mailOptions = {
       from: 'ksw09157.5212@gmail.com',
       to: receiver,
       subject: '이메일 제목',
-      text: 'Node.js의 Nodemailer + Google SMTP를 사용해서 보낸 메일입니다!',
+      text: 'Node.js의 Nodemailer + Google SMTP를 사용해서 보낸 메일입니다!', // 메일 내용(텍스트 형식)
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+    try {
+      // 메일 전송
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent: ' + info.response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
@@ -66,5 +75,5 @@ const tempUser: RegisterUserDTO = {
   email: receiver,
 };
 
-UserService.register(tempUser);
-UserService.sendMail();
+UserService.sendMailWithOAuth(tempUser);
+UserService.sendMailWithPassword();
